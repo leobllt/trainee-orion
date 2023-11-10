ecbf68467b454a26fbf074667c842ce6d300a3ac
leobllt <leonardo.bellant@gmail.com>
2023-11-09 23:30:51 -0300

    organizando

---
+++ src/datasource.py
@@ -0,0 +1,13 @@
+# Arquivo com a classe que obtém e repassa dados lidos para as demais
+
+class DataSource:
+    def __init__(self):
+        pass
+
+    # Método só para testar como seriam os dados
+    # se fossem lidos de um arquivo local.
+    def dadosTeste():
+        arq = open("src/dadosTeste.txt", 'r')
+        dados = arq.read().splitlines()
+        arq.close()
+        return dados
